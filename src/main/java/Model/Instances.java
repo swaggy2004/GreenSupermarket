b94117eca074efce1c898cbc7c33d8e7060db9a2
Swaggy <101066701+swaggy2004@users.noreply.github.com>
2023-12-17 00:44:35 +0530

    Update Instances.java
    Removed an unnecessary instance

--- src/main/java/Model/Instances.java
+++ src/main/java/Model/Instances.java
@@ -10,12 +10,7 @@ import java.util.*;
  */
 public class Instances {
     public List <ManagerAccount> ManagerAccounts = new ArrayList<ManagerAccount>();
-<<<<<<< HEAD
     public List<Product> products = new ArrayList<Product>();
-    public List<String> productIDs = new ArrayList<String>();
-=======
-      
->>>>>>> c271de9cc23485d6cdb4e0c68889ca621e1e66dc
 }
 
 
